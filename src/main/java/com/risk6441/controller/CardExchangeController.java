e1b2ad6edf49507e9a6e9bbc2b1a02f00a5987f6
BeePi <karthikbeepi@gmail.com>
2018-10-31 11:22:17 -0400

    CardExchangeContoller created.

---
+++ src/main/java/com/risk6441/controller/CardExchangeController.java
@@ -0,0 +1,33 @@
+package com.risk6441.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CardExchangeController {
+
+
+	  @FXML
+	    private Label lblPlayer;
+
+	    @FXML
+	    private Button btnTrade;
+
+	    @FXML
+	    private Button btnCancel;
+
+	    @FXML
+	    private Label lblTitle;
+
+	    @FXML
+	    void cancelTrade(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void trade(ActionEvent event) {
+
+	}
+
+}

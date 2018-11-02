d65a498cae9209ac866e39f828aa97077875d380
niravjdn <niravjdn@gmail.com>
2018-11-02 00:18:46 -0400

    Better Distribution of Cards

--- src/main/java/com/risk6441/gameutils/GameUtils.java
+++ src/main/java/com/risk6441/gameutils/GameUtils.java
@@ -126,10 +126,22 @@ public class GameUtils {
 		Stack<Card> stackOfCards = new Stack<Card>();
 
 		List<Territory> allterritories = getTerritoryList(map);
-	
+		ArrayList<CardKind> cardsRandaomList = new ArrayList<CardKind>();
+		
+		int eachUniqueCards = allterritories.size() / 3;
+		cardsRandaomList.addAll(Collections.nCopies(eachUniqueCards, CardKind.valueOf("CAVALRY")));
+		cardsRandaomList.addAll(Collections.nCopies(eachUniqueCards, CardKind.valueOf("ARTILLERY")));
+		cardsRandaomList.addAll(Collections.nCopies(eachUniqueCards, CardKind.valueOf("INFANTRY")));
+		int diff = allterritories.size() - cardsRandaomList.size();
+		if(diff > 0) {
+			for(int i=0; i < diff; i++) {
+				System.out.println("inside");
+				cardsRandaomList.add(CardKind.values()[(int) (Math.random() * CardKind.values().length)]);
+			}
+		}
 		int i = 0;
 		for (Territory territory : allterritories) {
-			Card card = new Card(CardKind.values()[(int) (Math.random() * CardKind.values().length)]);
+			Card card = new Card(cardsRandaomList.get(i++));
 			card.setTerritoryToWhichCardBelong(territory);
 			stackOfCards.push(card);
 		}

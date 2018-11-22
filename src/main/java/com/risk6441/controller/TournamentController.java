871439355090d1a4d4f385a15c64807bac4af00f
BeePi <karthikbeepi@gmail.com>
2018-11-22 07:21:45 -0500

    Tournament controller added!

---
+++ src/main/java/com/risk6441/controller/TournamentController.java
@@ -0,0 +1,95 @@
+package com.risk6441.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+
+public class TournamentController {
+
+    @FXML
+    private TextArea txtAreaConsole;
+
+    @FXML
+    private AnchorPane ttlPane;
+
+    @FXML
+    private TextArea txtAreaResult;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private ComboBox<?> comboGames;
+
+    @FXML
+    private ComboBox<?> comboTurns;
+
+    @FXML
+    private Button btnMap2;
+
+    @FXML
+    private Button btnMap3;
+
+    @FXML
+    private Button btnMap4;
+
+    @FXML
+    private Button btnMap5;
+
+    @FXML
+    private Button btnMap1;
+
+    @FXML
+    private ComboBox<?> comboP1;
+
+    @FXML
+    private ComboBox<?> comboP2;
+
+    @FXML
+    private ComboBox<?> comboP3;
+
+    @FXML
+    private ComboBox<?> comboP4;
+
+    @FXML
+    void actionMap1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionMap2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionMap3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionMap4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionMap5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitTournament(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playTournament(ActionEvent event) {
+
+    }
+
+}

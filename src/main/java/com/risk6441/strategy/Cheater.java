8dffd0685f8dfa336ab28961c39c044de1cf1b91
Charles <charlesjebalitherson@gmail.com>
2018-12-01 10:31:59 -0500

    Cheater Army Changes

--- src/main/java/com/risk6441/strategy/Cheater.java
+++ src/main/java/com/risk6441/strategy/Cheater.java
@@ -28,79 +28,97 @@ import javafx.scene.control.TextArea;
  */
 public class Cheater extends Observable implements IStrategy {
 
-	/* (non-Javadoc)
-	 * @see com.risk6441.strategy.IStrategy#reinforcementPhase(javafx.collections.ObservableList, com.risk6441.entity.Territory, javafx.scene.control.TextArea, com.risk6441.entity.Player)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.risk6441.strategy.IStrategy#reinforcementPhase(javafx.collections.
+	 * ObservableList, com.risk6441.entity.Territory, javafx.scene.control.TextArea,
+	 * com.risk6441.entity.Player)
 	 */
 	@Override
-	public void reinforcementPhase(ObservableList<Territory> territoryList, Territory territory,
-			Player currentPlayer,ArrayList<Territory> terrArList,ArrayList<Territory> adjTerrArList) {
-		
-		for(Territory terr : terrArList) {
+	public void reinforcementPhase(ObservableList<Territory> territoryList, Territory territory, Player currentPlayer,
+			ArrayList<Territory> terrArList, ArrayList<Territory> adjTerrArList) {
+
+		for (Territory terr : terrArList) {
 			terr.setArmy(terr.getArmy() * 2);
-			GameUtils.addTextToLog("Armies Doubled to "+terr.getArmy()+ " on "+ terr.getName()+"\n");
+			GameUtils.addTextToLog("Armies Doubled to " + terr.getArmy() + " on " + terr.getName() + "\n");
 		}
 		currentPlayer.setArmies(0);
 	}
 
-	/* (non-Javadoc)
-	 * @see com.risk6441.strategy.IStrategy#attackPhase(javafx.scene.control.ListView, javafx.scene.control.ListView, com.risk6441.models.PlayerModel, javafx.scene.control.TextArea)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.risk6441.strategy.IStrategy#attackPhase(javafx.scene.control.ListView,
+	 * javafx.scene.control.ListView, com.risk6441.models.PlayerModel,
+	 * javafx.scene.control.TextArea)
 	 */
 	@Override
 	public void attackPhase(ListView<Territory> terrList1, ListView<Territory> adjTerrList1, PlayerModel playerModel,
-			List<Player> playerList,ArrayList<Territory> terrArList,ArrayList<Territory> adjTerrArList) throws InvalidGameActionException {
-		
+			List<Player> playerList, ArrayList<Territory> terrArList, ArrayList<Territory> adjTerrArList)
+			throws InvalidGameActionException {
+
 		this.addObserver(playerModel);
 		Iterator<Territory> terrListIterator = terrArList.iterator();
 		while (terrListIterator.hasNext()) {
 			Territory attackingTerr = terrListIterator.next();
 			List<Territory> deffTerrList = getDefendingTerr(attackingTerr);
 			if (deffTerrList.size() > 0) {
-				for(Territory deffTerr : deffTerrList) {
-					if(attackingTerr.getArmy()>3)
-					{deffTerr.setArmy(attackingTerr.getArmy()/2);
-					attackingTerr.setArmy(attackingTerr.getArmy()/2);
-					GameUtils.addTextToLog(attackingTerr.getName()+ "("+attackingTerr.getPlayer().getName()+""
-							+ ") attacking on "+deffTerr+"("+deffTerr.getPlayer().getName()+")\n");
+				for (Territory deffTerr : deffTerrList) {
+					deffTerr.setArmy((attackingTerr.getArmy() / 2) + 1);
+					attackingTerr.setArmy((attackingTerr.getArmy() / 2) + 1);
+					GameUtils.addTextToLog(attackingTerr.getName() + "(" + attackingTerr.getPlayer().getName() + ""
+							+ ") attacking on " + deffTerr + "(" + deffTerr.getPlayer().getName() + ")\n");
 					deffTerr.getPlayer().getAssignedTerritory().remove(deffTerr);
 					deffTerr.setPlayer(attackingTerr.getPlayer());
-					
+
 					attackingTerr.getPlayer().getAssignedTerritory().add(deffTerr);
-					GameUtils.addTextToLog(deffTerr.getName() + " is conquered by "
-							+ attackingTerr.getPlayer().getName() + "\n");
-					playerModel.setNumOfTerritoryWon(playerModel.getNumOfTerritoryWon()+1);
-					Platform.runLater(()->{
+					GameUtils.addTextToLog(
+							deffTerr.getName() + " is conquered by " + attackingTerr.getPlayer().getName() + "\n");
+					playerModel.setNumOfTerritoryWon(playerModel.getNumOfTerritoryWon() + 1);
+					Platform.runLater(() -> {
 						setChanged();
 						notifyObservers("oneAttackDoneForCheater");
 					});
-				}}
+
+				}
 			} else {
 				continue;
 			}
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see com.risk6441.strategy.IStrategy#fortificationPhase(javafx.scene.control.ListView, javafx.scene.control.ListView, javafx.scene.control.TextArea, com.risk6441.entity.Player, com.risk6441.entity.Map)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.risk6441.strategy.IStrategy#fortificationPhase(javafx.scene.control.
+	 * ListView, javafx.scene.control.ListView, javafx.scene.control.TextArea,
+	 * com.risk6441.entity.Player, com.risk6441.entity.Map)
 	 */
 	@Override
 	public boolean fortificationPhase(ListView<Territory> selectedTerritory, ListView<Territory> adjTerritory,
-			Player currentPlayer, Map map,ArrayList<Territory> terrArList,ArrayList<Territory> adjTerrArList) {
-		for(Territory terr: terrArList) {
+			Player currentPlayer, Map map, ArrayList<Territory> terrArList, ArrayList<Territory> adjTerrArList) {
+		for (Territory terr : terrArList) {
 			List<Territory> adjDefTerrList = getDefendingTerr(terr);
-			if(adjDefTerrList!=null && adjDefTerrList.size()>0) {
-				GameUtils.addTextToLog("Doubled the army on territory :"+terr.getName()+"\n");
+			if (adjDefTerrList != null && adjDefTerrList.size() > 0) {
+				GameUtils.addTextToLog("Doubled the army on territory :" + terr.getName() + "\n");
 				terr.setArmy(terr.getArmy() * 2);
 			}
 		}
 		return true;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.risk6441.strategy.IStrategy#hasAValidAttackMove(javafx.scene.control.ListView, javafx.scene.control.TextArea)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.risk6441.strategy.IStrategy#hasAValidAttackMove(javafx.scene.control.
+	 * ListView, javafx.scene.control.TextArea)
 	 */
 	@Override
 	public boolean hasAValidAttackMove(ArrayList<Territory> territories) {
-		boolean isValidAttackMove =false;
+		boolean isValidAttackMove = false;
 		for (Territory territory : territories) {
 			if (getDefendingTerr(territory).size() > 0) {
 				isValidAttackMove = true;
